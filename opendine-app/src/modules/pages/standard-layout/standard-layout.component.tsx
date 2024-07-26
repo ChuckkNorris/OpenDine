940229b7072708a9c8bcc4992b8e74a1b79b2326
Levi Fuller <lfuller941@gmail.com>
2024-07-26 09:53:19 -0600

    chore: removed unused code from standard layout

--- opendine-app/src/modules/pages/standard-layout/standard-layout.component.tsx
+++ opendine-app/src/modules/pages/standard-layout/standard-layout.component.tsx
@@ -1,9 +1,7 @@
 import { AppBar, Container, Toolbar } from '@mui/material';
-import { Outlet, RouterProvider } from 'react-router-dom';
-// import HideOnScroll from 'modules/common/components/hide-on-scroll/hide-on-scroll.component';
+import { Outlet } from 'react-router-dom';
 import { Box, styled } from '@mui/system';
 import StandardLayoutNavigation from 'modules/pages/standard-layout/standard-layout.navigation';
-// import { ButtonLink } from 'modules/common/components/button-link/button-link.component';
 
 const ToolbarFill = styled('div')({ flexGrow: 1});
 const PageWrapper = styled('div')((props) => ({ padding: props.theme.spacing(4) }));
@@ -11,7 +9,6 @@ const PageWrapper = styled('div')((props) => ({ padding: props.theme.spacing(4) 
 export const StandardLayout = () => {
   return (
     <>
-      {/* <HideOnScroll> */}
         <AppBar position='sticky'>
           <Container maxWidth="xl">
             <Toolbar disableGutters>
@@ -20,25 +17,10 @@ export const StandardLayout = () => {
               </Box>
               <ToolbarFill />
               <StandardLayoutNavigation />
-              {/* {navMenuOptions.map((navOption, idx) => (
-                // Elements generated as dynamic lists should have a unique `key` property value (often the index)
-                // so that React can optimize which DOM elements it updates as state/props change
-                <ButtonLink
-                  isNavLink
-                  key={idx}
-                  buttonProps={{color: "secondary"}}
-                  sx={{ mr: 2 }}
-                  to={navOption.to}
-                >
-                  {navOption.text}
-                </ButtonLink>
-              ))} */}
             </Toolbar>
           </Container>
         </AppBar>
-      {/* </HideOnScroll> */}
       <PageWrapper>
-        {/* <RouterProvider router={standardLayoutRouter} /> */}
         {/* Child route components will be rendered here */}
         <Outlet />
       </PageWrapper>
