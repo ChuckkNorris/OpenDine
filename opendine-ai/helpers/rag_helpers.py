1767cf60eeb1ea1066515b72bf88c22d24f7aaed
Levi Fuller <lfuller941@gmail.com>
2024-10-01 07:50:03 -0600

    ai: chunked example data into separate vector buckets for testing

--- opendine-ai/helpers/rag_helpers.py
+++ opendine-ai/helpers/rag_helpers.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Union
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community import embeddings
@@ -5,36 +6,58 @@ from langchain_community import embeddings
 from langchain.text_splitter import CharacterTextSplitter
 
 class RagWebLoader:
-  def __init__(self, model_local, urls, template="Answer the following questions {topic}"):
-    self.urls = urls
+  def __init__(self, model_local, template="Answer the following questions {topic}"):
+    # self.urls = urls
     self.model_local = model_local
     self.template = template
 
-  def get_vector_store_for_urls(self, urls):
+  def get_doc_splits_from_urls(self, urls):
     docs = [WebBaseLoader(url).load() for url in urls]
     docs_list = [item for sublist in docs for item in sublist]
     text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
     doc_splits = text_splitter.split_documents(docs_list)
+    return doc_splits
 
-    vectorstore = Chroma.from_documents(
-      documents=doc_splits,
-      collection_name="bbc_news",
-      embedding=embeddings.OllamaEmbeddings(model='nomic-embed-text'),
-    )
-    return vectorstore.as_retriever()
+  def get_vector_store_for_urls(self, categorized_doc_splits: Dict[str, List[str]]):
+    vectorStore: Chroma = None
+    for category, doc_splits in categorized_doc_splits.items():
+      vectorStore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name=category,
+        embedding=embeddings.OllamaEmbeddings(model='nomic-embed-text')
+      ) if vectorStore is None else vectorStore.from_documents(
+        documents=doc_splits,
+        collection_name=category,
+        embedding=embeddings.OllamaEmbeddings(model='nomic-embed-text')
+      )
+    return vectorStore.as_retriever()
+    # docs = [WebBaseLoader(url).load() for url in urls]
+    # docs_list = [item for sublist in docs for item in sublist]
+    # text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
+    # doc_splits = text_splitter.split_documents(docs_list)
 
-  def invoke_rag_chain(self, query):
+    # vectorstore = Chroma.from_documents(
+    #   documents=doc_splits,
+    #   collection_name="ozempic",
+    #   embedding=embeddings.OllamaEmbeddings(model='nomic-embed-text'),
+    # ).from_documents(
+    #   documents=doc_splits,
+    #   collection_name="china",
+    #   embedding=embeddings.OllamaEmbeddings(model='nomic-embed-text'),
+    # )
+
+  def invoke_rag_chain(self, query, retriever):
     from langchain_community.chat_models import ChatOllama
     from langchain_core.runnables import RunnablePassthrough
     from langchain_core.output_parsers import StrOutputParser
     from langchain_core.prompts import ChatPromptTemplate
 
-    retriever = self.get_vector_store_for_urls(self.urls)
+    # retriever = self.get_vector_store_for_urls(self.urls)
 
     template = ChatPromptTemplate([
-            ("system", "You are an angry chatbot. If relevant to the query, please prefer using this context in your answers: {context}"), # Your name is {name}."),
+            ("system", "You are an angry chatbot. If relevant to the question, please prefer using this context in your answers: {context}"), # Your name is {name}."),
             ("human", "Hello, how are you doing?"),
-            ("ai", "I'm doing absolutely spectacular! Thank you SOOOO much for asking!"),
+            ("ai", "I feel how you look. Thank you SOOOO much for asking!"),
             ("human", "{question}"),
         ])
     rag_prompt = ChatPromptTemplate.from_template(self.template)
--- opendine-ai/rag_test.py
+++ opendine-ai/rag_test.py
@@ -7,18 +7,34 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
 from helpers.rag_helpers import RagWebLoader
+import sys
 
 model_local = ChatOllama(model="llama3.1:8B", device="cuda")
 
 # Chunk data
-urls = [
+ozempicUrls = [
   'https://www.bbc.com/news/live/c04p5916k4xt',
   'https://www.cbsnews.com/news/ozempic-wegovy-price-novo-nordisk-senate-committee-hearing/',
+  'https://www.lawsuit-information-center.com/ozempic-gastroparesis-lawsuit.html',
 ]
 
+chinaUrls = [
+  'https://www.britannica.com/place/China',
+  # 'https://www.bbc.com/news/live/c04p5916k4xt',
+  # 'https://www.cbsnews.com/news/ozempic-wegovy-price-novo-nordisk-senate-committee-hearing/',
+  # 'https://www.lawsuit-information-center.com/ozempic-gastroparesis-lawsuit.html',
+]
+
+# Get the first parameter to the python exe
+prompt = sys.argv[1] if len(sys.argv) > 1 else 'What is the population of China?'
+
 # retriever = get_vector_store_for_urls(urls)
-loader = RagWebLoader(model_local, urls)
-loader.invoke_rag_chain('You are an angry chatbot. Is Ozempic expensive compared to other countries?')
+loader = RagWebLoader(model_local)
+retriever = loader.get_vector_store_for_urls({
+  "ozempic": loader.get_doc_splits_from_urls(ozempicUrls),
+  "china": loader.get_doc_splits_from_urls(chinaUrls),
+})
+loader.invoke_rag_chain(prompt, retriever)
 
 # print("Before RAG\n")
 # before_rag_template = "What is {topic}"
