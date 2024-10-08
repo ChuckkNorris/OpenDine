0e6285a84fb04cbb35343d8fd40a9c51ec991cc5
Levi Fuller <lfuller941@gmail.com>
2024-10-08 09:56:45 -0600

    ai: progress on autograd

--- opendine-ai/micrograd-ai.py
+++ opendine-ai/micrograd-ai.py
@@ -46,17 +46,102 @@ print('d1', d1)
 print('d2', d2)
 print('slope', (d2 - d1)/h)
 # %%
-a = Value(2.0, label='a')
-b = Value(-3.0, label='b')
-c = Value(10.0, label='c')
-e = a*b; e.label = 'e'
-d = e + c; d.label = 'd'
+# Example Grad Function
+def run_example_grad() -> Value:
+  h = 0.001
+  a = Value(2.0, label='a')
+  b = Value(-3.0, label='b')
+  c = Value(10.0, label='c')
+  e = a*b; e.label = 'e'
+  d = e + c; d.label = 'd'
+  f = Value(-2.0, label='f')
+  l = d*f; l.label = 'l'
+  L1 = l.data
 
-print('d', d)
-print('d.prev', d._prev)
-print('d._op', d._op)
+  a = Value(2.0, label='a')
+  b = Value(-3.0, label='b')
+  c = Value(10.0, label='c')
+  # c.data +=h
+  e = a*b; e.label = 'e'
+  e.data += h
+  d = e + c; d.label = 'd'
+  # d.data += h
+  f = Value(-2.0, label='f')
+  l = d*f; l.label = 'l'
+  L2 = l.data
+
+  l.grad = 1.0
+  f.grad = 4.0
+  d.grad = -2
+  c.grad = -2.0
+  e.grad = -2.0
+
+  print((L2 - L1)/h)
+  return l
+l = run_example_grad()
+draw_dot(l)
+# What is the derivative of L with respect to L
+# If we change L, but a tiny amount (e.g. h), how much does L change (changes by h, proportial, derivative would be 1)
+
+# print('d', d)
+# print('d.prev', d._prev)
+# print('d._op', d._op)
+# %%
+# What we know
+# l = d * f
+# what is dl/dd = ?
+# dk/dd = f
+#(f(x+h)-f(x))/h
+#((d+h)*f - d*f)/h
+#(d*f + h*f - d*f) / h
+#(h*f) / h
+#f
+# f.grad = 4.0
+# d.grad = -2
+
+# Relationship between L and C?
+# dd / dc = 1.0
+# dd / de = 1.0
+d = c + e
+(f(x+h) - f(x)) / h
+((c+h + e) - (c + e)) / h
+(c + h + e - c - e)/h
+h/h
+# Local derivative - want to find d at end of graph, only knows it is a plus
+# Plus knows local influence on d and c, and d and e
+# Chain rule in calculus
 # %%
 # Draw value nodes
-draw_dot(d)
+
+
+
 
 # %%
+# Begin back propogation
+# Calculate derivative of a node with respect to other nodes
+# Need to know how weights
+# Interested in the derivative of the output,
+# Need to derive dl/dc
+
+
+# Calculate Derivatives between distant nodes
+# WANT:
+# dl / dc
+
+# KNOW:
+# dl / dd = -2.0
+# dd / dc = 1.0
+
+# Chain Rule:
+# dl / dc = (dl / dd) * (dd / dc)
+# dl / dc = -2.0 * 1.0
+# dl / dc = -2.0
+# Plus nodes ROUTE the gradient
+# 1 * (dl/dd) = -2.0
+
+# LAST NODE
+# dl / de = -2.0
+# dl / da = (dl / de) * (de / da)
+# dl / da = -2.0 * -3.0
+d = 4.00010
+l = -8.0020
--- opendine-ai/micrograd/value.py
+++ opendine-ai/micrograd/value.py
@@ -2,6 +2,8 @@
 class Value:
   def __init__(self, data, _children=(), _op='', label='') -> None:
     self.data = data
+    # default, we assume it's not affecting output initially
+    self.grad = 0
     self._prev = set(_children)
     self._op = _op
     self.label = label
